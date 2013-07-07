6a561df276e9895adfa3ef518bc96c603937119b
koki <sikibuton@outlook.com>
2013-07-08 00:27:11 +0900

    ちゃんと更新できてない？？？ 理由不明

--- iden.py
+++ iden.py
@@ -15,37 +15,54 @@ def f(x):
     y = -x*x
     return y
 
+def max_value_i(l):
+    a = max(l)
+    i=0
+    for i in range(len(l)):
+        if l[i] == a :
+           n = i
+    return n
 
 def main():
     N = 6
-    G = 100
+    G = 3
     n = []
     for i in range(N):
         n.append(random.uniform(-100,100))
     n_c = n
-    print "n="
+    print "first n="
     print n
     i=0
     j=0
     k=0
     while j<G:
+        print "j="
+        print j
         m = []
+        i=0
         for i in range(N):
             m.append((f(n[i])))
+        print"m="
+        print m
+        k=0
         for k in range (3):
-            a = max(xrange(len(m)), key=lambda i: m[i])
-            n[k] = n[a]
+            a = max_value_i(m)
             m[a] = min(m)
-        print n
+            n[k] = n[a]
+            print "k="
+            print k
+            print m
         n[3] = random.uniform(-100,100)
-        n[4] = (n[0]+n[1])/2
-        n[5] = (n[0]+n[2])/2
+        n[4] = abs((n[0]-n[1])/2)
+        n[5] = abs((n[0]+n[1])/2)
+        print"new n="
         print n
         j = j+1
     m = []
+    i=0
     for i in range(N):
         m.append(f(n[i]))
-    a = max(xrange(len(m)), key=lambda i: m[i])
+    a = max_value_i(m)
     print n[a]
 
 
